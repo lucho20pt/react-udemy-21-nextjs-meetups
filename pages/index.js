3739e3c8217acb090e797149df738f1588218a6e
Daniel Batista <dani_batista20@hotmail.com>
2022-03-28 23:31:47 +0100

    upd: get data 'react way' useEffect(), useState

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,42 @@
 // our-domain.com/
+import { useEffect, useState } from 'react'
 import MeetupList from '@/components/meetups/MeetupList'
 
-const DUMMY_MEETUP = [
+const DUMMY_MEETUPS = [
   {
     id: '123',
     title: 'The First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg/1920px-%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg/1920px-%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg',
     address: 'Street address, 123',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
   },
   {
     id: '456',
     title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg/1920px-%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg/1920px-%D0%97%D0%B8%D0%BC%D0%BD%D0%B8%D0%B9_%D0%A2%D0%B0%D0%B3%D0%B0%D0%BD%D0%B0%D0%B9.jpg',
     address: 'Street address, 456',
-    description: 'More dummy lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    description:
+      'More dummy lorem ipsum dolor sit amet consectetur adipisicing elit.'
   }
 ]
 
 const HomePage = () => {
   //
+  const [loadingMeetups, setLoadingMeetups] = useState([])
+
+  useEffect(() => {
+    // send a http req and fetch data
+    setLoadingMeetups(DUMMY_MEETUPS)
+    // return () => console.log('cleanup')
+  }, [])
+
   return (
     <>
       <h1>Home Page</h1>
-      <MeetupList meetups={DUMMY_MEETUP} />
+      <MeetupList meetups={loadingMeetups} />
     </>
   )
 }
